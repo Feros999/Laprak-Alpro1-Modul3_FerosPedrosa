e1234e6effcce5e7e065c642077cbdcaff1365fe
Feros999 <ferospedrosa@gmail.com>
2024-10-27 08:21:33 +0700

    nilaix.go

--- nilaix.go
+++ nilaix.go
@@ -6,11 +6,9 @@ import (
 )
 
 func calculateX(fx float64) (float64, error) {
-	// Cek apakah fx + 5 = 0, yang akan membuat pembagian menjadi tidak valid
 	if fx == -5 {
 		return 0, fmt.Errorf("nilai f(x) tidak boleh sama dengan -5 karena menghasilkan pembagian dengan nol")
 	}
-	// Hitung nilai x dari persamaan f(x)
 	x := (2 / (fx + 5)) + 5
 	return x, nil
 }
@@ -23,7 +21,6 @@ func main() {
 		log.Fatal("Input tidak valid")
 	}
 
-	// Hitung nilai x berdasarkan f(x)
 	x, err := calculateX(fx)
 	if err != nil {
 		log.Fatal(err)
