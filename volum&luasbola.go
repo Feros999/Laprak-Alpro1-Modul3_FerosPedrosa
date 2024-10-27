f6e129e3e2688945d0fd14f5e526fb16cb5b8eda
Feros999 <ferospedrosa@gmail.com>
2024-10-27 08:24:09 +0700

    volum&luasbola.go

--- volum&luasbola.go
+++ volum&luasbola.go
@@ -10,13 +10,9 @@ func main() {
 	fmt.Print("Masukkan jari-jari bola: ")
 	fmt.Scan(&radius)
 
-	// Konstanta pi
 	const pi float64 = 3.1415926535
 
-	// Hitung volume bola
 	volume := (4.0 / 3.0) * pi * math.Pow(float64(radius), 3)
-
-	// Hitung luas kulit bola
 	area := 4.0 * pi * math.Pow(float64(radius), 2)
 
 	fmt.Printf("Bola dengan jejari %d memiliki volume: %.2f\n", radius, volume)
